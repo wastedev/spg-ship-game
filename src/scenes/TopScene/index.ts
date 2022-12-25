0f17f798a927568612a3ea9841f4bcb4333daaab
wastedev <a.shabalinski@gmail.com>
2022-12-26 02:13:50 +0300

    FEAT: 100-500-80 ready

--- src/scenes/TopScene/index.ts
+++ src/scenes/TopScene/index.ts
@@ -12,6 +12,9 @@ export class TopScene extends Scene {
   private bottomBarrier!: MatterJS.BodyType;
   private backgroundIcebergs!: GameObjects.Image;
 
+  private goalStage!: number;
+  private goalStageRectangle!: Phaser.Physics.Matter.Sprite;
+
   constructor() {
     super('top-scene');
   }
@@ -67,8 +70,8 @@ export class TopScene extends Scene {
     this.goalZone.setDepth(-1);
 
     this.goalZone.setOnCollide(() => {
-      GAME_SPEEDS[MOVEMENT_SPEED] = GAME_SPEEDS[MOVEMENT_SPEED] / 3;
-      GAME_SPEEDS[ROTATION_SPEED] = GAME_SPEEDS[ROTATION_SPEED] / 2;
+      GAME_SPEEDS[MOVEMENT_SPEED] = 0.39;
+      GAME_SPEEDS[ROTATION_SPEED] = 0.16;
 
       this.cameras.main.pan(this.player.x, this.player.y, 1500);
       this.cameras.main.zoomTo(1.5, 1500);
@@ -82,6 +85,79 @@ export class TopScene extends Scene {
 
   update(): void {
     this.player.update();
+
+    if (this.goalZone.body) {
+      if (
+        this.player.x >= this.goalZone?.x - 50 &&
+        (this.player.y >= this.goalZone?.y - 30 || this.player.y <= this.goalZone?.y + 30)
+      ) {
+        this.goalStage = 1000;
+        this.goalZone.destroy();
+      }
+    }
+
+    if (this.goalStage > 500) {
+      if (
+        this.player.x >= this.goalStageRectangle?.x - 30 &&
+        (this.player.y >= this.goalStageRectangle?.y - 30 ||
+          this.player.y <= this.goalStageRectangle?.y + 30)
+      ) {
+        this.goalStage = 500;
+      }
+    }
+
+    if (this.goalStage > 80) {
+      if (
+        this.player.x >= this.goalStageRectangle?.x + 30 &&
+        (this.player.y >= this.goalStageRectangle?.y - 30 ||
+          this.player.y <= this.goalStageRectangle?.y + 30)
+      ) {
+        this.goalStage = 80;
+      }
+    }
+
+    if (this.goalStage < 80) {
+      if (
+        this.player.x >= this.goalStageRectangle?.x + 20 &&
+        (this.player.y >= this.goalStageRectangle?.y - 30 ||
+          this.player.y <= this.goalStageRectangle?.y + 30)
+      ) {
+        this.scene.start('side-scene');
+      }
+    }
+
+    switch (this.goalStage) {
+      case 1000:
+        this.goalStageRectangle = this.matter.add.sprite(
+          window.game.scale.width - window.game.scale.width / 4,
+          window.game.scale.height / 2,
+          'goal-distance',
+        );
+        this.goalStageRectangle.setDisplaySize(250, 100);
+        this.goalStageRectangle.setSensor(true);
+
+        --this.goalStage;
+
+        GAME_SPEEDS[MOVEMENT_SPEED] = 0.18;
+        GAME_SPEEDS[ROTATION_SPEED] = 0.08;
+
+        break;
+
+      case 500:
+        this.goalStageRectangle.setX(this.goalStageRectangle.x + 20);
+        this.goalStageRectangle.setDisplaySize(220, 70);
+
+        --this.goalStage;
+
+        break;
+
+      case 80:
+        this.goalStageRectangle.setX(this.goalStageRectangle.x + 30);
+        this.goalStageRectangle.setDisplaySize(200, 50);
+
+        --this.goalStage;
+        break;
+    }
   }
 
   initEnemies(): void {

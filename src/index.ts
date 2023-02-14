917ea4888fca41a0fb22262eff6dd2df8929c784
Fima <kolyshkin.efim@mail.ru>
2023-02-15 01:18:59 +0300

    normal level stagging

--- src/index.ts
+++ src/index.ts
@@ -28,7 +28,7 @@ const gameConfig: Types.Core.GameConfig = {
   physics: {
     default: 'matter',
     matter: {
-      debug: true,
+      // debug: true,
       setBounds: true,
       gravity: { y: 0 },
     },
